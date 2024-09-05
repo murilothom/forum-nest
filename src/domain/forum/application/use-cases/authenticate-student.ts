05026510012f95325320cb199fe6e2fb7e467074
Murilo Thom <murilothom.2002@hotmail.com>
2024-09-05 09:26:33 -0300

    feat: mapper

--- src/domain/forum/application/use-cases/authenticate-student.ts
+++ src/domain/forum/application/use-cases/authenticate-student.ts
@@ -1,8 +1,6 @@
 import { Either, left, right } from '@/core/either'
 import { Injectable } from '@nestjs/common'
-import { Student } from '../../enterprise/entities/student'
 import { StudentsRepository } from '../repositories/students-repository'
-import { HashGenerator } from '../cryptography/hash-generator'
 import { HashComparer } from '../cryptography/hash-comparer'
 import { Encrypter } from '../cryptography/encrypter'
 import { WrongCredentialsError } from './errors/wrong-credentials-error'
